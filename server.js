3fe1d59bb51b18a38ea97dede47923f11e962e8e
Rubs Robles <rubsrc@gmail.com>
2016-12-01 20:56:28 -0600

    hello world with express

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const http = require('http');
+'use strict';
+
+const express = require('express');
+const app =  express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/contacts', (request, response) => {
+	response.send('hello world!');
+});
 
 const hostname = 'localhost';
 const port = 3001;
 
-const server = http.createServer((request, response) => {
-	response.statusCode = 200;
-	response.setHeader('Content-Type', 'text/html');
-	response.end('<h1>Hello world!</h1>');
-} );
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
 	console.log(`Server is runnig at http://${hostname}:${port}`);
 });
