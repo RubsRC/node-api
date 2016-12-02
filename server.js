a773fd9d2598ebe75a0ed1bfc0ff0d052654d1a2
Rubs Robles <rubsrc@gmail.com>
2016-12-01 21:25:34 -0600

    added method get for contacts

--- server.js
+++ server.js
@@ -4,12 +4,31 @@ const express = require('express');
 const app =  express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+let contacts = require('./data');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/api/contacts', (request, response) => {
-	response.send('hello world!');
+app.get('/api/contacts/getContacts', (request, response) => {
+	if (!contacts) {
+		response.status(404).json({ message: 'No contacts found.'});
+	}
+	response.json(contacts);
+});
+
+app.get('/api/contacts/getContact/:id', (request, response) => {
+
+	const requestId = request.params.id;
+
+	let contact = contacts.filter(contact => {
+		return contact.id == requestId;
+	});
+
+	if (!contact[0] ) {
+		response.status(404).json({ message: 'No contact found.'});
+	} 
+	
+	response.json(contact[0]);
 });
 
 const hostname = 'localhost';
